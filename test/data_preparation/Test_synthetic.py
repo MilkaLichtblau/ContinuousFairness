fb2af916acc1038dc78a48a3aa9dcb73be148532
Meike Zehlike <meike.zehlike@tu-berlin.de>
2018-09-28 12:49:17 +0200

    set up test environment

---
+++ test/data_preparation/Test_synthetic.py
@@ -0,0 +1,26 @@
+'''
+Created on Sep 28, 2018
+
+@author: mzehlike
+'''
+import unittest
+
+
+class Test(unittest.TestCase):
+
+
+    def setUp(self):
+        pass
+
+
+    def tearDown(self):
+        pass
+
+
+    def testName(self):
+        pass
+
+
+if __name__ == "__main__":
+    # import sys;sys.argv = ['', 'Test.testName']
+    unittest.main()
