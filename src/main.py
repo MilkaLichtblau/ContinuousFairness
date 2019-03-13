3ef2de87dc1947ed4c6ba4f2cddf49ada1cba1d7
Meike Zehlike <meike.zehlike@tu-berlin.de>
2019-03-13 19:15:30 +0100

    encoding fixed

--- src/main.py
+++ src/main.py
@@ -8,72 +8,76 @@ import pandas as pd
 import numpy as np
 import argparse
 
-from data_preparation import *
+from data_preparation import synthetic, LSAT
 from visualization.plots import plotKDEPerGroup
 from util.util import scoresByGroups
-from cfa import cfa
-from argparse import ArgumentError
+# from cfa import cfa
 from cfa.cfa import ContinuousFairnessAlgorithm
 
 
 def createSyntheticData(size):
     nonProtectedAttributes = ['score']
     protectedAttributes = {"gender": 2, "ethnicity": 3}
-    creator = synthetic.SyntheticDatasetCreator(size, protectedAttributes, nonProtectedAttributes)
+    creator = synthetic.SyntheticDatasetCreator(
+        size, protectedAttributes, nonProtectedAttributes)
     creator.createTruncatedIntegerScoresNormallyDistributed(1, 101)
-    creator.writeToCSV('../data/synthetic/dataset.csv', '../data/synthetic/groups.csv')
-    plotKDEPerGroup(creator.dataset, creator.groups, 'score', '../data/synthetic/scoreDistributionPerGroup', '')
+    creator.writeToCSV('../data/synthetic/dataset.csv',
+                       '../data/synthetic/groups.csv')
+    plotKDEPerGroup(creator.dataset, creator.groups, 'score',
+                    '../data/synthetic/scoreDistributionPerGroup.png', '')
 
 
 def createLSATDatasets():
     creator = LSAT.LSATCreator('../data/LSAT/law_data.csv.xlsx')
     # gender and ethnicity in one dataset
     creator.prepareAllInOneData()
-    creator.writeToCSV('../data/LSAT/all/allInOneLSAT.csv', '../data/LSAT/all/allInOneGroups.csv')
-    plotKDEPerGroup(creator.dataset, creator.groups, 'LSAT', '../data/LSAT/all/scoreDistributionPerGroup_All_LSAT', '')
-    plotKDEPerGroup(creator.dataset, creator.groups, 'ZFYA', '../data/LSAT/all/scoreDistributionPerGroup_All_ZFYA', '')
+    creator.writeToCSV('../data/LSAT/all/allInOneLSAT.csv',
+                       '../data/LSAT/all/allInOneGroups.csv')
+    plotKDEPerGroup(creator.dataset, creator.groups, 'LSAT',
+                    '../data/LSAT/all/scoreDistributionPerGroup_All_LSAT', '')
+    plotKDEPerGroup(creator.dataset, creator.groups, 'ZFYA',
+                    '../data/LSAT/all/scoreDistributionPerGroup_All_ZFYA', '')
 
     # all ethnicity in one dataset
     creator.prepareAllRaceData()
-    creator.writeToCSV('../data/LSAT/allRace/allEthnicityLSAT.csv', '../data/LSAT/allRace/allEthnicityGroups.csv')
-    plotKDEPerGroup(creator.dataset, creator.groups, 'LSAT', '../data/LSAT/allRace/scoreDistributionPerGroup_AllRace_LSAT', '')
-    plotKDEPerGroup(creator.dataset, creator.groups, 'ZFYA', '../data/LSAT/allRace/scoreDistributionPerGroup_AllRace_ZFYA', '')
+    creator.writeToCSV('../data/LSAT/allRace/allEthnicityLSAT.csv',
+                       '../data/LSAT/allRace/allEthnicityGroups.csv')
+    plotKDEPerGroup(creator.dataset, creator.groups, 'LSAT',
+                    '../data/LSAT/allRace/scoreDistributionPerGroup_AllRace_LSAT', '')
+    plotKDEPerGroup(creator.dataset, creator.groups, 'ZFYA',
+                    '../data/LSAT/allRace/scoreDistributionPerGroup_AllRace_ZFYA', '')
 
     # gender dataset
     creator.prepareGenderData()
-    creator.writeToCSV('../data/LSAT/gender/genderLSAT.csv', '../data/LSAT/gender/genderGroups.csv')
-    plotKDEPerGroup(creator.dataset, creator.groups, 'LSAT', '../data/LSAT/gender/scoreDistributionPerGroup_Gender_LSAT', '')
-    plotKDEPerGroup(creator.dataset, creator.groups, 'ZFYA', '../data/LSAT/gender/scoreDistributionPerGroup_Gender_ZFYA', '')
+    creator.writeToCSV('../data/LSAT/gender/genderLSAT.csv',
+                       '../data/LSAT/gender/genderGroups.csv')
+    plotKDEPerGroup(creator.dataset, creator.groups, 'LSAT',
+                    '../data/LSAT/gender/scoreDistributionPerGroup_Gender_LSAT', '')
+    plotKDEPerGroup(creator.dataset, creator.groups, 'ZFYA',
+                    '../data/LSAT/gender/scoreDistributionPerGroup_Gender_ZFYA', '')
 
 
-def rerank_with_cfa(score_ranges, score_stepsize, thetas, result_dir, pathToData,
-                    pathToGroups, qual_attr, prot_attr):
+def rerank_with_cfa(score_stepsize, thetas, result_dir, pathToData,
+                    pathToGroups, qual_attr):
     data = pd.read_csv(pathToData, sep=',')
     groups = pd.read_csv(pathToGroups, sep=',')
 
     # check that we have a theta for each group
     if groups.shape[0] != len(thetas):
-        raise ArgumentError("invalid number of thetas. Specify one theta per group.")
+        raise ValueError(
+            "invalid number of thetas, should be {numThetas} Specify one theta per group.".format(numThetas=groups.shape[0]))
 
     regForOT = 5e-3
 
     cfa = ContinuousFairnessAlgorithm(data,
                                       groups,
-                                      prot_attr,
                                       qual_attr,
-                                      score_ranges,
                                       score_stepsize,
                                       thetas,
                                       regForOT,
                                       path=result_dir,
                                       plot=True)
     cfa.continuousFairnessAlgorithm()
-
-
-def parseScoreRanges(scoreString):
-    score_ranges = np.array(scoreString.split(","))
-    int_ranges = [int(i) for i in score_ranges]
-    return int_ranges
 
 
 def parseThetas(thetaString):
@@ -92,10 +96,10 @@ def main():
                         choices=['synthetic', 'lsat'],
                         help="creates datasets from raw data and writes them to disk")
     parser.add_argument("--run",
-                        nargs=5,
-                        metavar=('DATASET', 'SCORE RANGES', 'STEPSIZE', 'THETAS', 'DIRECTORY'),
-                        help="runs continuous fairness algorithm for given DATASET with SCORE RANGES \
-                              in STEPSIZE and THETAS and stores results into DIRECTORY")
+                        nargs=4,
+                        metavar=('DATASET', 'STEPSIZE', 'THETAS', 'DIRECTORY'),
+                        help="runs continuous fairness algorithm for given DATASET with \
+                              STEPSIZE and THETAS and stores results into DIRECTORY")
     parser.add_argument("--dir",
                         nargs=1,
                         type=str,
@@ -110,19 +114,16 @@ def main():
     elif args.create == ['lsat']:
         createLSATDatasets()
     elif args.run:
-        score_ranges = parseScoreRanges(args.run[1])
-        score_stepsize = float(args.run[2])
-        thetas = parseThetas(args.run[3])
-        result_dir = args.run[4]
+        score_stepsize = float(args.run[1])
+        thetas = parseThetas(args.run[2])
+        result_dir = args.run[3]
         if args.run[0] == 'synthetic':
-            rerank_with_cfa(score_ranges,
-                            score_stepsize,
+            rerank_with_cfa(score_stepsize,
                             thetas,
                             result_dir,
                             '../data/synthetic/dataset.csv',
                             '../data/synthetic/groups.csv',
-                            'score',
-                            ["gender", "ethnicity"])
+                            'score')
         elif args.run[0] == 'lsat_gender':
             # TODO: run experiments also with ZFYA
             rerank_with_cfa(score_ranges,
@@ -152,7 +153,8 @@ def main():
                             'LSAT',
                             ["sex", "race"])
         else:
-            parser.error("unknown dataset. Options are 'synthetic', 'lsat_gender', 'lsat_race, 'lsat_all'")
+            parser.error(
+                "unknown dataset. Options are 'synthetic', 'lsat_gender', 'lsat_race, 'lsat_all'")
     else:
         parser.error("choose one command line option")
 
