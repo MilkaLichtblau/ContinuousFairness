26717a9487c64366179133c6c1cf53824940a1fe
Meike Zehlike <meike.zehlike@tu-berlin.de>
2019-06-19 10:41:41 +0200

    added uuid to dataset

--- src/data_preparation/LSAT.py
+++ src/data_preparation/LSAT.py
@@ -14,7 +14,7 @@ excluding for now: region-first, sander-index, first_pf
 höchste ID: 27476
 
 '''
-
+import uuid
 import pandas as pd
 
 
@@ -44,6 +44,8 @@ class LSATCreator():
         data['LSAT'] = pd.to_numeric(data['LSAT'])
 
         data = data[['sex', 'LSAT', 'UGPA', 'ZFYA']]
+        data['uuid'] = [uuid.uuid4() for _ in range(len(data))]
+
         self.__groups = pd.DataFrame({"sex": [0, 1]})
         self.__dataset = data
 
@@ -90,37 +92,40 @@ class LSATCreator():
 #         data['UGPA'] = stats.zscore(data['UGPA'])
 
         data = data[['race', 'LSAT', 'UGPA', 'ZFYA']]
+        # add doc ids to identify documents later
+        data['uuid'] = [uuid.uuid4() for _ in range(len(data))]
+
         self.__groups = pd.DataFrame({"race": [0, 1, 2, 3, 4, 5, 6, 7]})
         self.__dataset = data
 
-    def prepareAllInOneData(self):
-        data = self.__origDataset.drop(columns=['region_first', 'sander_index', 'first_pf'])
-
-        data['sex'] = data['sex'].replace([2], 0)
-
-        data['race'] = data['race'].replace(to_replace="White", value=0)
-        data['race'] = data['race'].replace(to_replace="Amerindian", value=1)
-        data['race'] = data['race'].replace(to_replace="Asian", value=2)
-        data['race'] = data['race'].replace(to_replace="Black", value=3)
-        data['race'] = data['race'].replace(to_replace="Hispanic", value=4)
-        data['race'] = data['race'].replace(to_replace="Mexican", value=5)
-        data['race'] = data['race'].replace(to_replace="Other", value=6)
-        data['race'] = data['race'].replace(to_replace="Puertorican", value=7)
-
-        data['LSAT'] = data['LSAT'].apply(str)
-        data['LSAT'] = data['LSAT'].str.replace('.7', '.75', regex=False)
-        data['LSAT'] = data['LSAT'].str.replace('.3', '.25', regex=False)
-        data['LSAT'] = pd.to_numeric(data['LSAT'])
-
-        data = data[['sex', 'race', 'LSAT', 'UGPA', 'ZFYA']]
-        race = pd.Series([0, 1, 2, 3, 4, 5, 6, 7, 0, 1, 2, 3, 4, 5, 6, 7],
-                              index=[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15],
-                              name='race')
-        sex = pd.Series([0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1],
-                              index=[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15],
-                              name='sex')
-        self.__groups = pd.concat([sex, race], axis=1)
-        self.__dataset = data
+#     def prepareAllInOneData(self):
+#         data = self.__origDataset.drop(columns=['region_first', 'sander_index', 'first_pf'])
+#
+#         data['sex'] = data['sex'].replace([2], 0)
+#
+#         data['race'] = data['race'].replace(to_replace="White", value=0)
+#         data['race'] = data['race'].replace(to_replace="Amerindian", value=1)
+#         data['race'] = data['race'].replace(to_replace="Asian", value=2)
+#         data['race'] = data['race'].replace(to_replace="Black", value=3)
+#         data['race'] = data['race'].replace(to_replace="Hispanic", value=4)
+#         data['race'] = data['race'].replace(to_replace="Mexican", value=5)
+#         data['race'] = data['race'].replace(to_replace="Other", value=6)
+#         data['race'] = data['race'].replace(to_replace="Puertorican", value=7)
+#
+#         data['LSAT'] = data['LSAT'].apply(str)
+#         data['LSAT'] = data['LSAT'].str.replace('.7', '.75', regex=False)
+#         data['LSAT'] = data['LSAT'].str.replace('.3', '.25', regex=False)
+#         data['LSAT'] = pd.to_numeric(data['LSAT'])
+#
+#         data = data[['sex', 'race', 'LSAT', 'UGPA', 'ZFYA']]
+#         race = pd.Series([0, 1, 2, 3, 4, 5, 6, 7, 0, 1, 2, 3, 4, 5, 6, 7],
+#                               index=[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15],
+#                               name='race')
+#         sex = pd.Series([0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1],
+#                               index=[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15],
+#                               name='sex')
+#         self.__groups = pd.concat([sex, race], axis=1)
+#         self.__dataset = data
 
     def writeToCSV(self, pathToDataset, pathToGroups):
         self.__dataset.to_csv(pathToDataset, index=False, header=True)
