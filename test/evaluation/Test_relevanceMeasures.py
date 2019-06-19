1f68ed24f1984b471d016d218aef49d358e19aa0
Meike Zehlike <meike.zehlike@tu-berlin.de>
2019-06-19 17:57:35 +0200

    added tests

--- test/evaluation/Test_relevanceMeasures.py
+++ test/evaluation/Test_relevanceMeasures.py
@@ -54,6 +54,52 @@ class Test(unittest.TestCase):
 
         np.testing.assert_array_equal(expectedPak, actualPak)
 
+    def test_pak_fullyCorrectOrderSameScores(self):
+        # all items in origData and fairData are ordered in the same way
+        # precision at k should always be 1
+        origScoreCol = [20, 20, 20, 20, 20, 20, 20, 20, 20, 20, \
+                        10, 10, 10, 10, 10, 10, 10, 10, 10, 10]
+        fairScoreCol = [25, 25, 25, 25, 25, 25, 25, 25, 25, 25, \
+                        15, 15, 15, 15, 15, 15, 15, 15, 15, 15]
+        self._origData = self._origData.assign(score=origScoreCol)
+        self._fairData = self._fairData.assign(fairScore=fairScoreCol)
+
+        self._fairData = self._fairData.sort_values(by=['fairScore', 'uuid'], ascending=[False, True])
+        self._origData = self._origData.sort_values(by=['score', 'uuid'], ascending=[False, True])
+
+        fairIDs = self._fairData["uuid"].values
+        origIDs = self._origData["uuid"].values
+
+        for k in np.arange(self._dataSize):
+            self.assertEqual(1, pak(k + 1, fairIDs, origIDs), "failed at k={0}".format(k))
+
+    def test_pak_invertedOrderSameScores(self):
+        # all items in origData and fairData are ordered in reverse way
+        # precision at k should increase
+        origScoreCol = [20, 20, 20, 20, 20, 20, 20, 20, 20, 20, \
+                        10, 10, 10, 10, 10, 10, 10, 10, 10, 10]
+        fairScoreCol = [15, 15, 15, 15, 15, 15, 15, 15, 15, 15, \
+                        25, 25, 25, 25, 25, 25, 25, 25, 25, 25]
+
+        self._origData = self._origData.assign(score=origScoreCol)
+        self._fairData = self._fairData.assign(fairScore=fairScoreCol)
+
+        self._fairData = self._fairData.sort_values(by=['fairScore', 'uuid'], ascending=[False, True])
+        self._origData = self._origData.sort_values(by=['score', 'uuid'], ascending=[False, True])
+
+        fairIDs = self._fairData["uuid"].values
+        origIDs = self._origData["uuid"].values
+
+        expectedPak = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, \
+                       (2 / 11), (4 / 12), (6 / 13), (8 / 14), (10 / 15), \
+                       (12 / 16), (14 / 17), (16 / 18), (18 / 19), (20 / 20)]
+
+        actualPak = np.empty(self._dataSize)
+        for k in np.arange(self._dataSize):
+            actualPak[k] = pak(k + 1, fairIDs, origIDs)
+
+        np.testing.assert_array_equal(expectedPak, actualPak)
+
 
 if __name__ == "__main__":
     # import sys;sys.argv = ['', 'Test.test_pak']
